203e4e445acbee3d7baafcea1c98dca3eec69387
PaulObuba <paulobuba26@gmail.com>
2023-03-02 16:59:44 +0100

    file commit

--- routers/courses.js
+++ routers/courses.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 // Configure Multer for Uploading Image File
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/')
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
   },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
   // reject a file
-  if (file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+  if (file.mimetype === "image/jpg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
     cb(null, false);
@@ -24,10 +24,10 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
+  // limits: {
+  //   fileSize: 1024 * 1024 * 5,
+  // },
+  // fileFilter: fileFilter,
 });
 
 const { Courses } = require("../models/courses");
@@ -45,8 +45,11 @@ router.get("/", async (req, res) => {
 });
 
 // Post Data
-router.post("/", upload.single("coursesImage"), (req, res) => {
+router.post("/", upload.single("courseImage"), (req, res) => {
   console.log(req.file);
+
+  // res.redirect("/courses");
+
   const course = new Courses({
     image: req.file.path,
     title: req.body.title,
@@ -58,7 +61,7 @@ router.post("/", upload.single("coursesImage"), (req, res) => {
     .then(() => {
       res.status(201);
 
-      res.redirect('./courses');
+      res.redirect("/courses");
     })
     .catch((err) => {
       res.status(500).json({
@@ -98,6 +101,5 @@ router.post("/edit/:id", (req, res) => {
     }
   );
 });
-
 
 module.exports = router;
